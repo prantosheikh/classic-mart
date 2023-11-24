9b666410df46b85a802d1a08106b3fc8bc79c053
prantosheikh <prantosheikh834@gmail.com>
2023-11-24 20:03:18 +0600

    mern

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import AboutUs from "../pages/AboutUs/AboutUs";
+import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SingUp/SingUp";
 
@@ -6,10 +9,27 @@ import SignUp from "../pages/SingUp/SingUp";
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Login/>
+      element: <Main/>,
+      children : [
+
+        {
+          path: "/",
+          element: <Home />,
+        },
+        
+        {
+          path: "/signup",
+          element: <SignUp />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/about",
+          element: <AboutUs/>
+        },
+
+      ]
     },
-    {
-      path: "/signup",
-      element: <SignUp/>
-    }
   ]);
