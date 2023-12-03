02a65174253a4e2354a7bf7c351606593657020a
Pranto Sheikh <prantosheikh834@gmail.com>
2023-12-03 23:08:29 +0600

    some changes

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -8,7 +8,6 @@ const Main = () => {
       <Navbar />
       <Outlet />
       <Footer />
-      
     </div>
   );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -13,6 +13,8 @@ import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 
+
+
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
