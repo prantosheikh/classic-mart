bf06286352ec5c8e0700ba3b404e68311d4c0d4c
Pranto Sheikh <prantosheikh834@gmail.com>
2023-12-04 23:49:07 +0600

    good night

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
    };
 
    return (
-      <div className='flex gap-4'>
+      <div className='bg-white  flex gap-4'>
          <Swiper
             spaceBetween={30}
             centeredSlides={true}
@@ -39,10 +39,10 @@ const Hero = () => {
             className="mySwiper"
          >
             <SwiperSlide>
-               <img src="https://i.ibb.co/6n55x1D/GOODNESS-WEB-1280x.webp" alt="" />
+               <img className='rounded-lg' src="https://i.ibb.co/6n55x1D/GOODNESS-WEB-1280x.webp" alt="" />
             </SwiperSlide>
             <SwiperSlide>
-               <img src="https://i.ibb.co/mHZLGB7/2-627ee5b6-fca7-400c-b043-b3a824686756-1280x.webp" alt="" />
+               <img className='rounded-lg' src="https://i.ibb.co/mHZLGB7/2-627ee5b6-fca7-400c-b043-b3a824686756-1280x.webp" alt="" />
             </SwiperSlide>
             <SwiperSlide>
                <img src="https://i.ibb.co/Yp7Zh66/1-86790f4f-133a-4041-9f28-0ba0674566dc-1280x.webp" alt="" />
@@ -55,8 +55,8 @@ const Hero = () => {
                <span ref={progressContent}></span>
             </div>
          </Swiper>
-         <div>
-            <img className='cursor-pointer' src="https://i.ibb.co/MNhYZw0/grailify-acbfdoyqv9-U-unsplash1.jpg" alt="" />
+         <div className='w-[36%]'>
+            <img className='cursor-pointer rounded-lg' src="https://i.ibb.co/5FkhsGD/Orange-Pink-Purple-Soft-Gradients-Gloss-Tech-business-Podcast-Cover-2.png" alt="" />
          </div>
       </div>
    );
