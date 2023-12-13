aab7e40222230d31ced6442f3fb93946c3f1e437
Pranto Sheikh <prantosheikh834@gmail.com>
2023-12-13 22:30:05 +0600

    collection data showing display

--- src/pages/Collections/Collections.jsx
+++ src/pages/Collections/Collections.jsx
@@ -26,13 +26,52 @@ const Collections = () => {
       <Container>
          <div className='pt-8'>
             <h3><Link to='/'>Home</Link> / {id}</h3>
+
+            <div className='flex justify-between items-center my-12'>
+               <h1 className='text-3xl'>{id}</h1>
+               <div className='flex flex-col'>
+
+
+                  <select className='border py-3 px-10 text-center' name="example" id="example">
+                     <option value="option1">ALL</option>
+                     <option value="option2">Option 2</option>
+                     <option value="option3">Option 3</option>
+                  </select>
+
+
+
+                  <select className='border py-3 px-10 text-center' name="example" id="example">
+                     <option value="option1">BEST SEELING</option>
+                     <option value="option2">Option 2</option>
+                     <option value="option3">Option 3</option>
+                  </select>
+
+               </div>
+            </div>
          </div>
          <div>
-            {offers?.map(offer => <div>
-               <div>
-                  <h1>Offer</h1>
-               </div>
-            </div>)}
+
+            <div className='grid grid-cols-5 gap-10'>
+               {offers?.map(offer =>
+               (<div className=' '>
+                  <div className="mt-6 w-full ">
+                     <figure>
+                        <img src={offer?.product?.img} alt="Shoes" />
+                     </figure>
+                     <div className="text-center mt-4">
+                        <div className='my-6'>
+                           <h2 className="">Shoes!</h2>
+                           <p>If a dog chews shoes whose shoes does he choose?</p>
+                        </div>
+                        <div className="card-actions justify-end">
+                           <button className="w-full border py-2">Buy Now</button>
+                           <button className="w-full text-green-500 font-semibold border border-green-300 hover:bg-gray-50 py-4 my-4">ADD TO CART</button>
+                        </div>
+                     </div>
+                  </div>
+               </div>))}
+            </div>
+
          </div>
       </Container>
    );
